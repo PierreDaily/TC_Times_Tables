563988de4a0ec8c9a3b26efddf3d31d770c8b9e8
Pierre <contact@pierredaily.com>
2021-06-23 18:42:16 +0800

    first working draft

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const today = new Date();
+const now = new Date();
 
 const weekEndDays = [0, 6];
 
@@ -27,20 +27,62 @@ function formatTime(date) {
   }`;
 }
 
+/**
+ *
+ * @param {number} timeLeft
+ * @param {string} time
+ */
+function displayTablesElement(timeLeft, time, parent) {
+  const li = document.createElement("li");
+  li.classList = "tables__item";
+
+  const countElem = document.createElement("span");
+  countElem.classList = "tables__count";
+  countElem.textContent = `${String(timeLeft)} min`;
+
+  const timeElem = document.createElement("span");
+  timeElem.classList = "tables__time";
+  timeElem.textContent = time;
+
+  if (parent) {
+    li.append(countElem, timeElem);
+    parent.append(li);
+  }
+}
+
+function formatTime(date) {
+  const hours = Math.floor(date.getHours());
+  const minutes = date.getMinutes();
+
+  return `${hours > 12 ? hours - 12 : hours}:${minutes} ${
+    hours > 12 ? "pm" : "am"
+  }`;
+}
+const ul = document.getElementsByClassName("tables")[0];
+
 fetch("./public/timestables.json")
   .then((data) => data.json())
   .then((data) => {
-    const timesTables = isWeekDay(today)
+    const timesTables = isWeekDay(now)
       ? data.weekDay.map(timesTables2Date)
       : data.weekEndAndPublicHoliday.map(timesTables2Date);
 
     const nextBusIndex = timesTables.findIndex((elem) => {
-      return today.valueOf() - elem.valueOf() <= 0;
+      return now.valueOf() - elem.valueOf() <= 0;
     });
 
-    const p1 = document.createElement("p");
-    p1.innerText = formatTime(timesTables[nextBusIndex]);
-    const p2 = document.createElement("p");
-    p2.innerText = formatTime(timesTables[nextBusIndex + 1]);
-    document.body.append(p1, p2);
+    const filterTables = timesTables.slice(nextBusIndex);
+
+    filterTables.forEach((date, idx) => {
+      const seconds = (date.valueOf() - now.valueOf()) / 1000;
+      const minutes = seconds / 60;
+      const minutesLeft = Math.floor(minutes) + 1;
+      console.log(minutesLeft + 1);
+      displayTablesElement(minutesLeft, formatTime(date), ul);
+      if (idx === 0) {
+        document.getElementsByClassName(
+          "timer__count"
+        )[0].textContent = `${String(minutesLeft)} minutes`;
+      }
+    });
   });
